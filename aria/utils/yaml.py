ceb64bbf5f463a4ae9fd129a65cc0597ff9fbda3
max-orlov <maxim@gigaspaces.com>
2017-12-04 16:10:25 +0200

    ARIA-422 Parsing CSARs is broken

--- aria/utils/yaml.py
+++ aria/utils/yaml.py
@@ -48,3 +48,8 @@ except ImportError:
 
 finally:
     from ruamel import yaml                                                     # pylint: disable=unused-import
+
+
+# Enables support for writing python unicode class with yaml. This is a Temporary fix which
+# should be replaced by a proper usage of ruamel.
+yaml.SafeLoader.add_constructor('tag:yaml.org,2002:python/unicode', lambda _, node: node.value)
